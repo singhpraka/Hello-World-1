f02454c7014ebe6c02bcc1885a5b64123f17560d
katemerifield <kgmerif@gmail.com>
2014-12-03 18:47:24 -0800

    added name and "sup?"

--- HelloWorld.java
+++ HelloWorld.java
@@ -24,6 +24,7 @@ public class HelloWorld {
 	        nameList.add("James Sovick");
 		nameList.add("Mathias Van Patten");
 		nameList.add("Michelle Dalton");
+		namelist.add("Kate Merifield");
 		
 		loopDaLoop(nameList, generator);
 	}
@@ -41,7 +42,7 @@ public class HelloWorld {
 						   "Yikes!  What's that ahead of me?",
 						   "I did it?","much hello wow",
 						   "Путин величайшим лидером мир когда-либо видел",
-						   "Whats good guys?");
+						   "Whats good guys?" "sup?");
 			int randNum = generator.nextInt(greetList.size());
 			
 			System.out.printf("%s says \"%s\"\n", name, greetList.get(randNum));
