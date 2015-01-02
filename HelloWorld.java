5a0badaac56ac2fb9a4e806c29729ee0116f5432
mdalton-spartronics <mdalton4915@olypm.com>
2015-01-02 12:40:30 -0800

    changing my greeting to test new version of Eclipse installed

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,36 +3,34 @@
 // Makes a list to add names to, then prints the list to the console with different greetings for each name
 import java.util.*;
 public class HelloWorld {
-  
-  public static void main(String[] Args){
-    ArrayList<String> nameList = new ArrayList<String>();
-    Random generator = new Random();
-    
-          nameList.add("Alumni: Faust");
-          nameList.add("Alumni: Keet Curtis");
-          nameList.add("Alumni: Ronnie Nigash");
-          nameList.add("Ben DeVries");
-          nameList.add("Binnur");
-          nameList.add("Jack Stratton");
-          nameList.add("Joe Van Dyke");
-          nameList.add("Mr. Chee");
-          nameList.add("Tarkan Al-Kazily");
-          nameList.add("Noah Levine");
-          nameList.add("Michaela Leung");
-          nameList.add("Clare Liguori");
-          nameList.add("Clint Pells");
-          nameList.add("James Sovick");
-          nameList.add("Mathias Van Patten");
-          nameList.add("Michelle Dalton");
-          nameList.add("Grantham Makowicz");
-          nameList.add("Braden Ploger");
-    
-    loopDaLoop(nameList, generator);
-  }
+	
+	public static void main(String[] Args){
+		ArrayList<String> nameList = new ArrayList<String>();
+		Random generator = new Random();
+		
+	        nameList.add("Alumni: Faust");
+	        nameList.add("Alumni: Keet Curtis");
+	        nameList.add("Alumni: Ronnie Nigash");
+	        nameList.add("Ben DeVries");
+	        nameList.add("Binnur");
+	        nameList.add("Jack Stratton");
+	        nameList.add("Joe Van Dyke");
+	        nameList.add("Mr. Chee");
+	        nameList.add("Tarkan Al-Kazily");
+	        nameList.add("Noah Levine");
+	        nameList.add("Michaela Leung");
+	        nameList.add("Clare Liguori");
+	        nameList.add("Clint Pells");
+	        nameList.add("James Sovick");
+		nameList.add("Mathias Van Patten");
+		nameList.add("Michelle Dalton");
+		
+		loopDaLoop(nameList, generator);
+	}
 
-  private static void loopDaLoop (ArrayList<String> list, Random generator){
-    for(String name : list){
-      List<String> greetList = Arrays.asList("hi!", "how are you?",
+	private static void loopDaLoop (ArrayList<String> list, Random generator){
+		for(String name : list){
+			List<String> greetList = Arrays.asList("hi!", "how are you?",
                                                    "did you do your homework?",
                                                    "good morning.", "what's up?",
                                                    "merhabalar!!", "welcome!",
@@ -40,11 +38,8 @@ public class HelloWorld {
 						   "allons-y!", "geronimo!", 
 						   "greetings, fellow being.",
 						   "hola", "bonjour!", "Aloha",
-						   "Yikes!  What's that ahead of me?",
-						   "I did it?","much hello wow",
-						   "Путин величайшим лидером мир когда-либо видел",
-						   "hello everyone!",
-						   "Whats good guys?");
+						   "I did it?","much hello wow", "Путин величайшим лидером мир когда-либо видел",
+						   "Eclipse me");
 			int randNum = generator.nextInt(greetList.size());
 			
 			System.out.printf("%s says \"%s\"\n", name, greetList.get(randNum));
